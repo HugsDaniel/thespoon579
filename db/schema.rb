ef783f57f0fc4955d76f43f18df8db8508388cef
Hugo Daniel <daniel.hugo@protonmail.com>
2021-04-27 09:32:34 +0200

    Add chef name to restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_27_071918) do
+ActiveRecord::Schema.define(version: 2021_04_27_072954) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_04_27_071918) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "address"
+    t.string "chef_name"
   end
 
 end
